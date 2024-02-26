3b13329b012cfc5ae6b5363886231274fda977d3
Bryan Trujillo <trujillobryan99@yahoo.com>
2024-02-25 23:17:48 -0500

    added product ratings&prices-titles

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -1,5 +1,27 @@
 import { IoCart } from "react-icons/io5"
+import { IoMdHeart } from "react-icons/io"
+import { FaStarHalfAlt, FaStar, FaRegStar } from "react-icons/fa"
+import { NavLink } from "react-router-dom"
+import { BodyOne } from "../common/CustomComponents"
 
+export const RenderRatingStars = (rating) => {
+    const totalStars = 5;
+    const fullStars = Math.floor(rating);
+    const hasHalfStars = rating % 1 !== 0;
+    const stars = [];
+
+    for (let i = 1; i < totalStars; i++) {
+        if(i <= fullStars) {
+            stars.push(<FaStar key={i} color="#ff8a00"/>);
+        } else if (hasHalfStars && i === fullStars + 1) { 
+            stars.push(<FaStarHalfAlt key="half-star" color="#ff8a00"/>);
+        } else {
+            stars.push(<FaRegStar key={i} color="#ff8a00"/>);
+            
+        }
+    }
+    return stars;
+}
 
 export const ProductCard = ({
     id,
@@ -41,7 +63,30 @@ export const ProductCard = ({
             <button className="add-to-cart-btn product-btn primary-btn">
                 <IoCart size={23}/>
             </button>
+            <button className="love-btn product-btn primary-btn">
+                <IoMdHeart size={23}/>
+            </button>
            </div>
+          </div>
+           <div className="details flex items-center flex-col bg-white pt-6">
+            <NavLink to={`/product-details/${id}`}>
+                <BodyOne>{title}</BodyOne>
+            </NavLink>
+            <div className="flex items-center gap-2 -mt-2 mb-2">
+              {RenderRatingStars(rating)}
+            </div>
+            <div className="flex items-center gap-3">
+               {price.slice(0, 1).map((priceItem, index) => (
+                <>
+                <BodyOne className="line-through" key={index}>
+                   ${priceItem.value}
+                </BodyOne>
+                <BodyOne className="text-primary-green" key={index}>
+                   ${(priceItem.value - (priceItem.value * discount) / 100).toFixed(2)}
+                </BodyOne>
+                </>
+               ))}
+            </div>
         </div>
     </div>
     </>
